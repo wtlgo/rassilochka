80e1182b3e14d15d801ea1cc8f8b213d3c88be26
wtlgo <georgeendo0@gmail.com>
2019-12-31 04:03:34 +0300

    Собственно, готово

---
+++ РАССЫЛОЧЬКА/config.hpp
@@ -0,0 +1,9 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+inline const char* v = "5.103";
+inline const char* access_token = "Асес токен";
+inline const char* message = "ФОШИСТ!";
+inline constexpr int chat_id = 1488;
+
+#endif
--- РАССЫЛОЧЬКА/main.cpp
+++ РАССЫЛОЧЬКА/main.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <string>
+#include <random>
+
+#include <wtlgo/Network.hpp>
+#include <nlohmann/json.hpp>
+
+using Network = wtlgo::Network;
+using json = nlohmann::json;
+
+#include "config.hpp"
 
 int main(int argc, const char * argv[]) {
+    const std::string vk_api = "https://api.vk.com/method/";
+
+    auto chat = json::parse(Network::instance().request(vk_api + "messages.getChat", {
+        {"access_token", access_token}, 
+        {"v", v}, 
+        {"chat_id", std::to_string(chat_id)}
+    }));
+
+    std::vector<int> users = chat["response"]["users"];
+
+    std::mt19937 engine((std::random_device())());
+    std::uniform_int_distribution<int64_t> random_id(0, std::numeric_limits<int64_t>::max());
+
+    for(auto user : users) {
+        if(user < 0) continue;
+
+        Network::instance().request(vk_api + "messages.send", {
+            {"access_token", access_token}, 
+            {"v", v}, 
+            {"user_id", std::to_string(user)},
+            {"message", message},
+            {"random_id", std::to_string(random_id(engine))}
+        });
+    }
+    
     return 0;
 }
