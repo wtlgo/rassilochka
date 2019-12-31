2d8516f9474cbd9573646343c5a1bbde1744f42e
wtlgo <georgeendo0@gmail.com>
2019-12-31 03:34:16 +0300

    Какие копирайты о чем речь вообще

--- РАССЫЛОЧЬКА/main.cpp
+++ РАССЫЛОЧЬКА/main.cpp
@@ -1,15 +1,5 @@
-//
-//  main.cpp
-//  РАССЫЛОЧЬКА
-//
-//  Created by George Endo on 31.12.2019.
-//  Copyright © 2019 George Endo. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
