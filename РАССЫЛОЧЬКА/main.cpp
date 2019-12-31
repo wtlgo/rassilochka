48e0b3aab755a5d5aca921725ea9378e6a0b3311
wtlgo <georgeendo0@gmail.com>
2019-12-31 03:32:51 +0300

    Initial Commit

---
+++ РАССЫЛОЧЬКА/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  РАССЫЛОЧЬКА
+//
+//  Created by George Endo on 31.12.2019.
+//  Copyright © 2019 George Endo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
